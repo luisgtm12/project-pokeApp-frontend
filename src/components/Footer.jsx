5166657a1e8738a41fa95c16ceea784a40477ae2
luisgtm12 <120407727+luisgtm12@users.noreply.github.com>
2024-07-25 13:25:10 -0500

    implementacion de css a Preloader y Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 function Footer(){
   return(
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} Mi App Pokémon. Todos los derechos reservados.</p>
+      <p>&copy; {new Date().getFullYear()} Mi PokéApp. Todos los derechos reservados.</p>
       <div className="footer__links">
-        <a href="#about">Acerca de</a>
-        <a href="#contact">Contacto</a>
-        <a href="#privacy">Política de Privacidad</a>
+        <a href="https://www.pokemon.com/es" target="_blank" rel="noopener noreferrer">Acerca de</a>
+        <a href="https://support.pokemon.com/hc/es" target="_blank" rel="noopener noreferrer">Contacto</a>
+        <a href="https://www.pokemon.com/es/legal/" target="_blank" rel="noopener noreferrer">Política de Privacidad</a>
       </div>
     </footer>
   )
